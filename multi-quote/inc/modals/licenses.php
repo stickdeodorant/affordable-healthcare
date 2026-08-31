d5566f26a2bc7bfc9fed2c36f07d787090b2d4b4
Christopher Ruby <stickdeodorant@gmail.com>
2026-08-31 08:04:07 -0400

    Update RM7 licensing and privacy legal text

--- multi-quote/inc/modals/licenses.php
+++ multi-quote/inc/modals/licenses.php
@@ -2,8 +2,7 @@
 		<div class="row">
 			<div class="col" style="max-width: 800px; margin: auto; padding: 30px;">
 			<h1 class="text-center">Health Insurance Licenses</h1>
-			<!--<?php /*<p class="pb-4">Producer FEIN: 87-1437350</p> */ ?>
-			<?php /* <p class="pb-4">Invitations for applications for insurance on this website are made through Joseph Leto, only where licensed and appointed. Joseph Leto’s National Producer Number is 19907913 and state license numbers are set forth below.</p> */ ?>-->
+			<p class="pb-3">Invitations for applications for insurance are made only where licensed and appointed. RM7 Services LLC National Producer Number (NPN): 19954493.</p>
 			<table class="table licensing-table text-left">
 				<tbody><tr>
 						<th class="p-2">
@@ -13,166 +12,51 @@
 								<h4 class="sub-section-title">License #</h4>
 						</th>
 				</tr>
-				<tr>
-						<td class="p-2">Alabama</td>
-						<td class="p-2">3001378732</td>
-				</tr>
-				<tr>
-						<td class="p-2">Arkansas</td>
-						<td class="p-2">3001378717</td>
-				</tr>
-				<tr>
-						<td class="p-2">Arizona</td>
-						<td class="p-2">3001378737</td>
-				</tr>
-				<tr>
-						<td class="p-2">Colorado</td>
-						<td class="p-2">698899</td>
-				</tr>
-				<tr>
-						<td class="p-2">Connecticut</td>
-						<td class="p-2">2527426</td>
-				</tr>
-				<tr>
-						<td class="p-2">Delaware</td>
-						<td class="p-2">3001378750</td>
-				</tr>
-				<tr>
-						<td class="p-2">Florida</td>
-						<td class="p-2">3001384761</td>
-				</tr>
-				<tr>
-						<td class="p-2">Georgia</td>
-						<td class="p-2">219474</td>
-				</tr>
-				<tr>
-					<td class="p-2">Idaho</td>
-					<td class="p-2">861345</td>
-				</tr>
-				<tr>
-					<td class="p-2">Illinois</td>
-					<td class="p-2">3001378757</td>
-				</tr>
-				<tr>
-						<td class="p-2">Iowa</td>
-						<td class="p-2">3001378672</td>
-				</tr>
-				<tr>
-						<td class="p-2">Indiana</td>
-						<td class="p-2">3691217</td>
-				</tr>
-				<tr>
-						<td class="p-2">Kansas</td>
-						<td class="p-2">871437350</td>
-				</tr>
-				<tr>
-						<td class="p-2">Kentucky</td>
-						<td class="p-2">19972758</td>
-				</tr>
-				<tr>
-						<td class="p-2">Louisiana</td>
-						<td class="p-2">919913</td>
-				</tr>
-				<tr>
-						<td class="p-2">Maryland</td>
-						<td class="p-2">3001378778</td>
-				</tr>
-				<tr>
-						<td class="p-2">Mississippi</td>
-						<td class="p-2">15041192</td>
-				</tr>
-				<tr>
-						<td class="p-2">Missouri</td>
-						<td class="p-2">3001383097</td>
-				</tr>
-				<tr>
-						<td class="p-2">Montana</td>
-						<td class="p-2">3001378548</td>
-				</tr>
-				<tr>
-						<td class="p-2">Nebraska</td>
-						<td class="p-2">3001378643</td>
-				</tr>
-				<tr>
-						<td class="p-2">Nevada</td>
-						<td class="p-2">3690259</td>
-				</tr>
-				<tr>
-						<td class="p-2">New Hampshire</td>
-						<td class="p-2">3001378639</td>
-				</tr>
-				<tr>
-						<td class="p-2">New Jersey</td>
-						<td class="p-2">3001378618</td>
-				</tr>
-				<tr>
-						<td class="p-2">New Mexico</td>
-						<td class="p-2">3001389831</td>
-				</tr>
-				<tr>
-						<td class="p-2">North Carolina</td>
-						<td class="p-2">3001378700</td>
-				</tr>
-				<tr>
-						<td class="p-2">North Dakota</td>
-						<td class="p-2">3001378529</td>
-				</tr>
-				<tr>
-						<td class="p-2">Oklahoma</td>
-						<td class="p-2">3001390887</td>
-				</tr>
-				<tr>
-						<td class="p-2">Oregon</td>
-						<td class="p-2">3001385317</td>
-				</tr>
-				<tr>
-						<td class="p-2">Pennsylvania</td>
-						<td class="p-2">1014081</td>
-				</tr>
-				<tr>
-						<td class="p-2">Rhode Island</td>
-						<td class="p-2">3001378579</td>
-				</tr>
-				<tr>
-						<td class="p-2">South Carolina</td>
-						<td class="p-2">3001378606</td>
-				</tr>
-				<tr>
-						<td class="p-2">South Dakota</td>
-						<td class="p-2">10028480</td>
-				</tr>
-				<tr>
-						<td class="p-2">Tennessee</td>
-						<td class="p-2">3001378695</td>
-				</tr>
-				<tr>
-						<td class="p-2">Texas</td>
-						<td class="p-2">2692024</td>
-				</tr>
-				<tr>
-						<td class="p-2">Utah</td>
-						<td class="p-2">861086</td>
-				</tr>
-				<tr>
-						<td class="p-2">Virginia</td>
-						<td class="p-2">151691</td>
-				</tr>
-				<tr>
-						<td class="p-2">Washington D.C.</td>
-						<td class="p-2">3001384761</td>
-				</tr>
-				<tr>
-						<td class="p-2">West Virginia</td>
-						<td class="p-2">3001378841</td>
-				</tr>
-				<tr>
-						<td class="p-2">Wisconsin</td>
-						<td class="p-2">3001378612</td>
-				</tr>
-				<tr>
-						<td class="p-2">Wyoming</td>
-						<td class="p-2">470446</td>
-				</tr>
+				<tr><td class="p-2">Alabama</td><td class="p-2">3001361522</td></tr>
+				<tr><td class="p-2">Arizona</td><td class="p-2">3001361514</td></tr>
+				<tr><td class="p-2">Arkansas</td><td class="p-2">3001361510</td></tr>
+				<tr><td class="p-2">California</td><td class="p-2">6005197</td></tr>
+				<tr><td class="p-2">Colorado</td><td class="p-2">697040</td></tr>
+				<tr><td class="p-2">Connecticut</td><td class="p-2">2729440</td></tr>
+				<tr><td class="p-2">DC</td><td class="p-2">3001361512</td></tr>
+				<tr><td class="p-2">Delaware</td><td class="p-2">3001361519</td></tr>
+				<tr><td class="p-2">Florida</td><td class="p-2">L113668</td></tr>
+				<tr><td class="p-2">Georgia</td><td class="p-2">219187</td></tr>
+				<tr><td class="p-2">Idaho</td><td class="p-2">858569</td></tr>
+				<tr><td class="p-2">Illinois</td><td class="p-2">3001361545</td></tr>
+				<tr><td class="p-2">Indiana</td><td class="p-2">3686682</td></tr>
+				<tr><td class="p-2">Iowa</td><td class="p-2">3001361677</td></tr>
+				<tr><td class="p-2">Kansas</td><td class="p-2">863985680-000</td></tr>
+				<tr><td class="p-2">Kentucky</td><td class="p-2">1141065</td></tr>
+				<tr><td class="p-2">Louisiana</td><td class="p-2">918545</td></tr>
+				<tr><td class="p-2">Maryland</td><td class="p-2">3001361533</td></tr>
+				<tr><td class="p-2">Michigan</td><td class="p-2">133581</td></tr>
+				<tr><td class="p-2">Minnesota</td><td class="p-2">40754534</td></tr>
+				<tr><td class="p-2">Mississippi</td><td class="p-2">15041112</td></tr>
+				<tr><td class="p-2">Missouri</td><td class="p-2">3001366713</td></tr>
+				<tr><td class="p-2">Montana</td><td class="p-2">3001361559</td></tr>
+				<tr><td class="p-2">Nebraska</td><td class="p-2">3001361655</td></tr>
+				<tr><td class="p-2">Nevada</td><td class="p-2">3685394</td></tr>
+				<tr><td class="p-2">New Hampshire</td><td class="p-2">3001361674</td></tr>
+				<tr><td class="p-2">New Jersey</td><td class="p-2">3001361628</td></tr>
+				<tr><td class="p-2">New Mexico</td><td class="p-2">3001389079</td></tr>
+				<tr><td class="p-2">New York</td><td class="p-2">LA-1674921</td></tr>
+				<tr><td class="p-2">North Carolina</td><td class="p-2">3001361726</td></tr>
+				<tr><td class="p-2">North Dakota</td><td class="p-2">3001361563</td></tr>
+				<tr><td class="p-2">Ohio</td><td class="p-2">1370149</td></tr>
+				<tr><td class="p-2">Oklahoma</td><td class="p-2">3001361590</td></tr>
+				<tr><td class="p-2">Oregon</td><td class="p-2">3001407202</td></tr>
+				<tr><td class="p-2">Pennsylvania</td><td class="p-2">1010818</td></tr>
+				<tr><td class="p-2">Rhode Island</td><td class="p-2">3001361598</td></tr>
+				<tr><td class="p-2">South Carolina</td><td class="p-2">3001361624</td></tr>
+				<tr><td class="p-2">South Dakota</td><td class="p-2">10028438</td></tr>
+				<tr><td class="p-2">Tennessee</td><td class="p-2">3001361679</td></tr>
+				<tr><td class="p-2">Texas</td><td class="p-2">2686479</td></tr>
+				<tr><td class="p-2">Utah</td><td class="p-2">858204</td></tr>
+				<tr><td class="p-2">Virginia</td><td class="p-2">151602</td></tr>
+				<tr><td class="p-2">West Virginia</td><td class="p-2">3001364585</td></tr>
+				<tr><td class="p-2">Wisconsin</td><td class="p-2">3001361642</td></tr>
+				<tr><td class="p-2">Wyoming</td><td class="p-2">469184</td></tr>
 		</tbody></table>
 			</div>
 		</div>
